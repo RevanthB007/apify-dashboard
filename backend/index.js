646b299364fa1f2216c6f20abb1cdc796d63e9cb
Revanth Bojja <revanthbojja12@gmail.com>
2025-08-06 22:31:47 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,31 @@
 import dotenv from "dotenv";
 import express from "express";
 import apifyRoutes from "./src/routes/route.js";
-import cors from "cors"; 
+import cors from "cors";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-const app = express()
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+const app = express();
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.json());
-app.use(cors(
-  {
-    origin: process.env.CORS_ORIGIN || "*",
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || origin === process.env.CORS_ORIGIN) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-  }
-));
+  })
+);
 
-
-app.use("/api",apifyRoutes);
+app.use("/api", apifyRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
