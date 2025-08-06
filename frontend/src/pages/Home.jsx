d37fb70364b96afca2dff90c2e0d8053836c285c
Revanth Bojja <revanthbojja12@gmail.com>
2025-08-06 23:25:26 +0530

    remove search bar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -35,7 +35,7 @@ export default function Home() {
         </div>
 
         {/* Search Bar */}
-        <div className="mb-6">
+        {/* <div className="mb-6">
           <div className="relative max-w-md">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
             <input
@@ -44,7 +44,7 @@ export default function Home() {
               className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition-all"
             />
           </div>
-        </div>
+        </div> */}
 
         {/* Stats */}
         <div className="mb-6">
